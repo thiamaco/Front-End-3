a88c659df4aa5498df132c5a7150871666614d87
Thiamaco <thiamaco.lucas55@gmail.com>
2023-11-01 16:28:37 -0300

    script orientado a objetos

--- script.js
+++ script.js
@@ -1,76 +1,169 @@
-var dados = [];
-var CoordAtualizados =[]
-
+var CoordAtualizados = []
+var array;
 const contatos = document.getElementById('contatos');
 const tableBody = document.getElementById('table-body');
 const pontos = document.getElementById('pontos');
 var bodyAniver = document.getElementById("body-aniver");
 var DiaAtual = new Date();
 // requsiçao para iniciar a pagina com os dados, se for utilizado um banco de dados aqui puxaria os dados do banco//
+$(window).ready(function(){
 fetch('/inicializacao')
-.then(response => response.json())
-.then(data => {
-  dados = data;
-    for(var i=0; i<dados.length;i++){
-      //converte o Timestamp para um objeto date
-      dados[i].dob.date = new Date(dados[i].dob.date);
-      var aniv =  new Date(2023,dados[i].dob.date.getMonth(),dados[i].dob.date.getDate())
-      var dias = Math.floor((aniv-DiaAtual) / (1000 * 60 * 60 * 24))
-        if(dias>=0){
-          dados[i].dob.aniversario=dias+1;
-        }else{
-        dados[i].dob.aniversario=(dias+365)+1;
-        }
-      render(dados[i])
-    }
+  .then(response => response.json())
+  .then(data => {
+    array = new Dados(data);
+  })
+  .catch(error => console.error('Erro:', error));
 })
-.catch(error => console.error('Erro:', error));
+//Iniciar();
+//orientada a objeto//
+function Dados(objeto) {
+  this.dado = objeto;
+  this.contatos;
+  this.Render = function (dado) {
+    tableBody.innerHTML = '';
+    for (var i = 0; i < dado.length; i++) {
+      const row = document.createElement('tr');
+      const age = document.createElement('td');
+      const gender = document.createElement('td');
+      const namefirst = document.createElement('td');
+      const namelast = document.createElement('td');
+      const phone = document.createElement('td');
+      const city = document.createElement('td');
+      const state = document.createElement('td');
+      const email = document.createElement('td');
+      const cpf = document.createElement('td');
+      age.textContent = dado[i].dob.age;
+      gender.textContent = dado[i].gender;
+      namefirst.textContent = dado[i].name.first + " " + dado[i].name.last;
+      phone.textContent = dado[i].phone;
+      city.textContent = dado[i].location.city
+      state.textContent = dado[i].location.state;
+      email.textContent = dado[i].email;
+      cpf.textContent = dado[i].id.value;
+      row.appendChild(namefirst);
+      row.appendChild(age);
+      row.appendChild(gender);
+      row.appendChild(city);
+      row.appendChild(state);
+      row.appendChild(email);
+      row.appendChild(cpf);
+      row.appendChild(phone);
+      tableBody.appendChild(row);
+    }
+  }
+  this.AtualizaDate = function (dado) {
+    for (var i = 0; i < dado.length; i++) {
+      dado[i].dob.date = new Date(dado[i].dob.date);
+      var aniv = new Date(2023, dado[i].dob.date.getMonth(), dado[i].dob.date.getDate())
+      var dias = Math.floor((aniv - DiaAtual) / (1000 * 60 * 60 * 24))
+      if (dias >= 0) {
+        dado[i].dob.aniversario = dias + 1;
+      } else {
+        dado[i].dob.aniversario = (dias + 365) + 1;
+      }
+    }
+  }
+  this.Inserir= function(dado,objeto){
+        dado.push(objeto)
+        this.AtualizaDate(dado);
+        this.Render(dado);
+  }
+  this.RenderContatos = function(lista){
+      this.contatos = lista;
+      const TableContatos = document.getElementById('contatos');
+      TableContatos.innerHTML = '';
+      for (var i = 0; i < lista.length; i++) {
+      const user = document.createElement('div');
+      const divimg = document.createElement('div');
+      const contato = document.createElement('div');
+      contato.classList.add('cont');
+      user.classList.add('dados');
+      const div = document.createElement('div');
+      const img = document.createElement('img');
+      const nome = document.createElement('h3');
+      const phone = document.createElement('h6');
+      const pais = document.createElement('h6');
+      img.src = lista[i].foto;
+      phone.textContent = lista[i].phone;
+      pais.textContent = lista[i].pais;
+      nome.textContent = lista[i].nome;
+      div.appendChild(nome)
+      div.appendChild(phone)
+      div.appendChild(pais)
+      contato.appendChild(div)
+      divimg.appendChild(img);
+      user.appendChild(divimg)
+      user.appendChild(contato)
+      contatos.append(user)
+      }
+  }
 
-//renderiza uma pessoa na tela home//
-function render(pessoa) {
-  const row = document.createElement('tr');
-  const age = document.createElement('td');
-  const gender = document.createElement('td');
-  const namefirst = document.createElement('td');
-  const namelast = document.createElement('td');
-  const phone = document.createElement('td');
-  const city = document.createElement('td');
-  const state = document.createElement('td');
-  const email = document.createElement('td');
-  const cpf = document.createElement('td');
-  age.textContent = pessoa.dob.age;
-  gender.textContent = pessoa.gender;
-  namefirst.textContent = pessoa.name.first + " " + pessoa.name.last;
-  phone.textContent = pessoa.phone;
-  city.textContent = pessoa.location.city
-  state.textContent = pessoa.location.state;
-  email.textContent = pessoa.email;
-  cpf.textContent = pessoa.id.value;
-  row.appendChild(namefirst);
-  row.appendChild(age);
-  row.appendChild(gender);
-  row.appendChild(city);
-  row.appendChild(state);
-  row.appendChild(email);
-  row.appendChild(cpf);
-  row.appendChild(phone);
-  tableBody.appendChild(row);
+  this.RenderAniversario = function (dado) {
+    bodyAniver.innerHTML = "";
+    for (var i = 0; i < dado.length; i++) {
+    const row = document.createElement('tr');
+    const nome = document.createElement('td');
+    const dia = document.createElement('td');
+    const mes = document.createElement('td');
+    const ano = document.createElement('td');
+    const aniv = document.createElement('td');
+    const nomesDosMeses = [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro"
+    ];
+    const mesv = dado[i].dob.date.getMonth();
+    dia.textContent = dado[i].dob.date.getDate()
+    mes.textContent = nomesDosMeses[mesv];
+    ano.textContent = dado[i].dob.date.getFullYear()
+    nome.textContent = dado[i].name.first + " " + dado[i].name.last;
+    aniv.textContent = dado[i].dob.aniversario;
+    row.appendChild(nome);
+    row.appendChild(dia);
+    row.appendChild(mes);
+    row.appendChild(ano);
+    row.appendChild(aniv);
+    bodyAniver.appendChild(row);
+  }
+  this.ordenarTabelaPorDia();
 }
+  this.ordenarTabelaPorDia =  function () {
+    var $tabela = $('.table-aniver');
+    var linhas = $tabela.find('tr').get();
+    var cabecalho = linhas.shift();
+    linhas.sort(function (a, b) {
+      var diaA = parseInt($(a).find('td:last-child').text());
+      var diaB = parseInt($(b).find('td:last-child').text());
+      return diaA - diaB;
+    });
+    bodyAniver.innerHTML = "";
+  
+    $tabela.append(cabecalho);
+    $.each(linhas, function (index, linha) {
+  
+      $tabela.append(linha);
+    });
+  }
+  this.AtualizaDate(this.dado);
+  this.Render(this.dado);
+}
+
+
 //evento para adicionar uma nova pessoa ao sistema, fazendo uma requisiçao add-dados//
 $('.add-user').on('click', function () {
   fetch('/add-dados')
     .then(response => response.json())
     .then(data => {
-      data.dob.date = new Date(data.dob.date);
-      var aniv =  new Date(2023,dados[i].dob.date.getMonth(),dados[i].dob.date.getDate())
-      var dias = Math.floor((aniv-DiaAtual) / (1000 * 60 * 60 * 24))
-        if(dias>=0){
-          dados[i].dob.aniversario=dias+1;
-        }else{
-        dados[i].dob.aniversario=(dias+365)+1;
-        }
-      dados.push(data)
-      render(data)
+      array.Inserir(array.dado, data);
       $("#message-add").fadeIn();
       setTimeout(function () {
         $("#message-add").fadeOut();
@@ -82,26 +175,24 @@ $('.add-user').on('click', function () {
 $("#btnsearch").on('click', function () {
   tableBody.innerHTML = '';
   var value = $('.pesquisar').val();
-    $.ajax({
-      type: 'POST',
-      url: '/search',
-      contentType: 'application/json',
-      data: JSON.stringify({ objeto: dados, valor: value }),
-      success: function (data) {
-        for (var i = 0; i < data.result.length; i++) {
-          render(data.result[i]);
-        }
-      },
-      error: function (xhr, status, error) {
-        console.error('Falha na solicitação:', error);
-      }
-    })
+  $.ajax({
+    type: 'POST',
+    url: '/search',
+    contentType: 'application/json',
+    data: JSON.stringify({ objeto: array.dado, valor: value }),
+    success: function (data) {
+      array.Render(data.result);
+    },
+    error: function (xhr, status, error) {
+      console.error('Falha na solicitação:', error);
+    }
+  })
 });
 //evento enter do input de pesquisa//
 $(document).ready(function () {
-  $(".pesquisar").keyup(function (e) {    
+  $(".pesquisar").keyup(function (e) {
     if (e.keyCode === 13) {
-    $("#btnsearch").click();
+      $("#btnsearch").click();
     }
   });
 });
@@ -117,17 +208,13 @@ $('.tabela-home').on("click", function () {
 });
 //evento a pagina de contatos, fazendo uma requisiçao
 $('.lista-contatos, .icon').on('click', function () {
-  contatos.innerHTML = '';
   $.ajax({
     type: 'POST',
     url: '/lista-contatos',
     contentType: 'application/json',
-    data: JSON.stringify({ objeto: dados }),
+    data: JSON.stringify({ objeto: array.dado }),
     success: function (data) {
-      console.log(data);
-      for (var i = 0; i < data.lista.length; i++) {
-        renderLista(data.lista[i]);
-      }
+      array.RenderContatos(data.lista);
       $('.icon').show();
       $('#contatos').show();
       $('.search').hide();
@@ -141,31 +228,6 @@ $('.lista-contatos, .icon').on('click', function () {
     }
   });
 });
-//renderiza os contato da lista de contato
-function renderLista(lista) {
-  const user = document.createElement('div');
-  const divimg = document.createElement('div');
-  const contato = document.createElement('div');
-  contato.classList.add('cont');
-  user.classList.add('dados');
-  const div = document.createElement('div');
-  const img = document.createElement('img');
-  const nome = document.createElement('h3');
-  const phone = document.createElement('h6');
-  const pais = document.createElement('h6');
-  img.src = lista.foto;
-  phone.textContent = lista.phone;
-  pais.textContent = lista.pais;
-  nome.textContent = lista.nome;
-  div.appendChild(nome)
-  div.appendChild(phone)
-  div.appendChild(pais)
-  contato.appendChild(div)
-  divimg.appendChild(img);
-  user.appendChild(divimg)
-  user.appendChild(contato)
-  contatos.append(user)
-}
 // renderiza um mapa com foco incial em porto alegre
 let map;
 async function initMap() {
@@ -179,47 +241,47 @@ async function initMap() {
 initMap();
 //evento para abrir um mapa na tela, exibindo a localizaçao de todos do sistema
 $('.street').on('click', function () {
-  ajustarcoord(dados)
-    $('#map').show();
-    $('#procurar').show();
-    $('.table').hide()
-    $('.search').hide();
-    $('#contatos').hide();
-    $('.icon').hide();
-    $('.table-aniver').hide();
+  ajustarcoord(array.dado)
+  $('#map').show();
+  $('#procurar').show();
+  $('.table').hide()
+  $('.search').hide();
+  $('#contatos').hide();
+  $('.icon').hide();
+  $('.table-aniver').hide();
 });
 //funcao para ajustar as coordenadas de acordo o nome da rua e a cidade da pessoa//
-function ajustarcoord(dados){
-  for(var i=0;i<dados.length;i++){
-    var loc = dados[i].location.street.name+', '+dados[i].location.city  
+function ajustarcoord(dados) {
+  for (var i = 0; i < dados.length; i++) {
+    var loc = dados[i].location.street.name + ', ' + dados[i].location.city
     var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ 'address': loc }, function(results, status) {
+    geocoder.geocode({ 'address': loc }, function (results, status) {
       if (status === google.maps.GeocoderStatus.OK) {
-        var locations ={}
-        locations.lat=results[0].geometry.location.lat();
-        locations.lng=results[0].geometry.location.lng();  
-          CoordAtualizados.push(locations);
+        var locations = {}
+        locations.lat = results[0].geometry.location.lat();
+        locations.lng = results[0].geometry.location.lng();
+        CoordAtualizados.push(locations);
       } else {
-          console.log( "erro pos:"+i);
-          CoordAtualizados.push('erro');
+        console.log("erro pos:" + i);
+        CoordAtualizados.push('erro');
       }
     });
   }
-  setTimeout(()=>{
-    for(var i=0;i<dados.length;i++){
-    if(CoordAtualizados[i]!='erro')
-      dados[i].location.coordinates = CoordAtualizados[i];
+  setTimeout(() => {
+    for (var i = 0; i < dados.length; i++) {
+      if (CoordAtualizados[i] != 'erro')
+        dados[i].location.coordinates = CoordAtualizados[i];
     }
-    if(i==dados.length){
+    if (i == dados.length) {
       rendermarcadores(dados);
       options(dados);
       atualizar();
     }
-  },2000)
+  }, 2000)
 }
 //renderiza os marcadores no mapa
 function rendermarcadores(objeto) {
-  for (var i = 0; i < objeto.length; i++) { 
+  for (var i = 0; i < objeto.length; i++) {
     new google.maps.Marker({
       position: objeto[i].location.coordinates,
       map: map,
@@ -235,6 +297,7 @@ function focusOnPoint(objeto, i) {
 }
 //renderiza as opcoes a ser selecionada para o usuario//
 function options(objeto) {
+  pontos.innerHTML = ""
   for (var i = 0; i < objeto.length; i++) {
     const option = document.createElement('option');
     option.innerText = objeto[i].name.first + ' ' + objeto[i].name.last
@@ -245,104 +308,29 @@ function options(objeto) {
 //evento quando há alguma alteraçao no select para alterar o foco no mapa
 $("#procurar").on("change", function () {
   var opcaoSelecionada = $(this).find("option:selected").val();
-  focusOnPoint(dados, opcaoSelecionada);
+  focusOnPoint(array.dado, opcaoSelecionada);
 });
 //funcao para atualizar os dados com as novas coordenadas no backend//
-function atualizar(){
+function atualizar() {
   $.ajax({
     type: 'POST',
     url: '/atualizar',
     contentType: 'application/json',
-    data: JSON.stringify(dados),
+    data: JSON.stringify(array.dado),
     error: function (xhr, status, error) {
       console.error('Falha na solicitação:', error);
     }
   })
 }
 //exibe os aniversarios das pessoas
-$('.lista-aniver').on('click',function(){
-  bodyAniver.innerHTML="";
+$('.lista-aniver').on('click', function () {
   $('#map').hide();
-    $('#procurar').hide();
-    $('.table').hide()
-    $('.search').hide();
-    $('#contatos').hide();
-    $('.icon').hide();
-    
+  $('#procurar').hide();
+  $('.table').hide()
+  $('.search').hide();
+  $('#contatos').hide();
+  $('.icon').hide();
   $('.table-aniver').show();
-  for(var i=0;i<dados.length;i++){
-    aniver(dados[i]);
-    if(i==dados.length-1){
-      ordenarTabelaPorDia();
-    }
-  }
-  
+  array.RenderAniversario(array.dado)  
+
 })
-
-/*setTimeout(()=>{
-//function falta(){
-  for(var i=0;i<dados.length;i++){
-    var aniv =  new Date(2023,dados[i].dob.date.getMonth(),dados[i].dob.date.getDate())
-    var dias = Math.floor((aniv-DiaAtual) / (1000 * 60 * 60 * 24))
-    if(dias>=0){
-      dados[i].dob.aniversario=(dias);
-    }else{
-    dados[i].dob.aniversario=(dias+365);
-    }
-  }
-//}
-},2000)
-*/
-//funcao para renderizar as datas na tela//
-function aniver(objeto) {
-  const row = document.createElement('tr');
-  const nome = document.createElement('td');
-  const dia = document.createElement('td');
-  const mes = document.createElement('td');
-  const ano = document.createElement('td');
-  const aniv = document.createElement('td');
-  const nomesDosMeses = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro"
-  ];
-  const mesv= objeto.dob.date.getMonth();
-  dia.textContent = objeto.dob.date.getDate()
-  mes.textContent = nomesDosMeses[mesv];
-  ano.textContent = objeto.dob.date.getFullYear()
-  nome.textContent = objeto.name.first + " " + objeto.name.last;
-  aniv.textContent = objeto.dob.aniversario;
-  row.appendChild(nome);
-  row.appendChild(dia);
-  row.appendChild(mes);
-  row.appendChild(ano);
-  row.appendChild(aniv);
-  bodyAniver.appendChild(row);
-}
-//ordena a tabela por faltante//
-function ordenarTabelaPorDia() {
-  var $tabela = $('.table-aniver');
-  var linhas = $tabela.find('tr').get();
-  var cabecalho = linhas.shift();
-  linhas.sort(function(a, b) {
-    var diaA = parseInt($(a).find('td:last-child').text());
-    var diaB = parseInt($(b).find('td:last-child').text());
-    return diaA - diaB;
-  });
-  bodyAniver.innerHTML="";
-
-  $tabela.append(cabecalho);
-  $.each(linhas, function(index, linha) {
-    
-    $tabela.append(linha);
-  });
-}
