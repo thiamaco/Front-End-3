adbc77398a5b702b7aa35e5d4d20521fac95258c
Thiamaco <thiamaco.lucas55@gmail.com>
2023-11-05 17:31:13 -0300

    versao handlebars

--- Front/app.js
+++ Front/app.js
@@ -2,25 +2,23 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const { engine } = require('express-handlebars');
-
 //app
 const app = express();
-
+//body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 //template
 app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set("views", "./views");
 app.use(express.json());
-
-var dados2 = [];
+var DiaAtual = new Date();
 
 // rotas dos arquivos//
 app.get('/style.css', (req, res) => {
   res.sendFile(__dirname + '/style.css');
 })
+
 app.get('/script.js', (req, res) => {
   res.sendFile(__dirname + '/script.js');
 })
@@ -30,7 +28,6 @@ app.get('/', async (req, res) => {
     .then(response => response.json())
     .then(data => {
       res.render('table_home', { dados: data });
-      dados2 = data;
     })
     .catch(error => {
       console.error('Erro ao buscar os dados:', error);
@@ -56,7 +53,6 @@ app.get('/add-dados', async (req, res) => {
 })
 
 app.get('/contatos', async (req, res) => {
-  const lista = [];
   fetch('http://localhost:3000/results')
     .then(response => response.json())
     .then(data => {
@@ -67,60 +63,35 @@ app.get('/contatos', async (req, res) => {
     })
 });
 
-app.get('/map', (req,res)=>{
-  res.render('map',{dados: dados2});
+app.get('/map', (req, res) => {
+  fetch('http://localhost:3000/results')
+  .then(response => response.json())
+  .then(data => {
+    res.render('map', { dados: data });
+  })
+  .catch(error => {
+    console.error('Erro ao buscar os dados:', error);
+  })
+  
 })
 
 app.get('/dados', async (req, res) => {
-  res.json(dados2);
-  console.log(dados2)
+  fetch('http://localhost:3000/results')
+    .then(response => response.json())
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      console.error('Erro ao buscar os dados:', error);
+    })
 });
-/*
-
-app.get('/home',(req,res)=>{
-
-  res.redirect('/')
-})
-
-app.get('/map', (req,res)=>{
-  res.render('map', dados);
-})
-
-
-
-app.get('/add-dados', async (req, res) => {
-  try {
-    const response = await axios.get('https://randomuser.me/api/?nat=BR');
-    const data = response.data;
-    dados.push(data.results);
-    res.json(data.results[0]);
-    //res.render('table_home', data);
-  } catch (error) {
-    res.status(500).json({ error: 'Ocorreu um erro na requisição.' });
-  }
-});
-//requisocoes post//
-app.post('/search', async (req, res) => {
-  const data = req.body;
-  console.log(data)
-  const results = [];
-  for (const obj of data.objeto) {
-    if (obj.gender.includes(data.valor) || obj.location.city.includes(data.valor) || obj.id.value.includes(data.valor)) {
-      results.push(obj);
-    } else if (data.valor == '') {
-      results.push(obj);
-    }
-  }
-  if (data != null) {
-    res.render('table_home', data);
-  } else {
-    res.status(500).json({ error: 'Ocorreu um erro na requisição.' });
-  }
-})
-
-
 
 app.get('/aniversarios', async (req, res) => {
+  fetch('http://localhost:3000/results')
+  .then(response => response.json())
+  .then(data => {
+  
+  
   const aniversarios = [];
   const nomesDosMeses = [
     "Janeiro",
@@ -135,36 +106,39 @@ app.get('/aniversarios', async (req, res) => {
     "Outubro",
     "Novembro",
     "Dezembro"
-];
-  for (const obj of dados) {
+  ];
+  for (var i = 0; i < data.length; i++) {
+    data[i].results[0].dob.date = new Date(data[i].results[0].dob.date);
     const aniv = {};
-    obj.dob.date = new Date(obj.dob.date);
-    aniv.dia = obj.dob.date.getDate()
-    aniv.mes = nomesDosMeses[obj.dob.date.getMonth()];
-    aniv.ano = obj.dob.date.getFullYear();
-    aniv.nome = obj.name.first + " " + obj.name.last;
-    aniv.aniversario = obj.dob.aniversario;
-    aniv.faltantes = obj.dob.faltantes;
-    
+    aniv.dia = data[i].results[0].dob.date.getDate()
+    aniv.mes = nomesDosMeses[data[i].results[0].dob.date.getMonth()];
+    aniv.ano = data[i].results[0].dob.date.getFullYear();
+    aniv.nome = data[i].results[0].name.first + " " + data[i].results[0].name.last;
+    var dias = Math.floor((new Date(2023, data[i].results[0].dob.date.getMonth(), aniv.dia) - DiaAtual) / (1000 * 60 * 60 * 24))
+      if (dias >= 0) {
+        aniv.aniversario = dias = dias + 1;
+      } else {
+        aniv.aniversario = dias = (dias + 365) + 1;
+      }
     aniversarios.push(aniv);
-    console.log(obj.dob.date.dia)
-  }
-  if (aniversarios != null) {
-    res.render('aniversarios', {aniversarios});
-  } else {
-    res.status(500).json({ error: 'Ocorreu um erro na requisição.' });
-  }
+    }
+  aniversarios.sort(compareNames);
+  res.render('aniversarios', { dados: aniversarios });
+
+})
+.catch(error => {
+  console.error('Erro ao buscar os dados:', error);
+})
 });
 
-app.post('/atualizar', async (req, res) => {
-  const data = req.body;
-  if (data != null) {
-    dados = data;
-  } else {
-    res.status(500).json({ error: 'Erro ao atualizar.' });
-  }
-});
-*/
+function compareNames(a, b) {
+  if (a.aniversario < b.aniversario) return -1;
+  if (a.aniversario > b.aniversario) return 1;
+  return 0;
+}
+
+
+
 //server//
 const port = 2000;
 app.listen(port, () => {
--- Front/script.js
+++ Front/script.js
@@ -1,25 +1,17 @@
 var dados = [];
+var results = []
 var CoordAtualizados =[]
 const pontos = document.getElementById('pontos');
 var DiaAtual = new Date();
+
 fetch('/dados')
 .then(response => response.json())
 .then(data => {
   dados = data;
-    for(var i=0; i<dados.length;i++){
-      dados[i].dob.date = new Date(dados[i].dob.date);
-      dados[i].dob.date.dia = dados[i].dob.date.getDate();
-      dados[i].dob.date.ano = dados[i].dob.date.getFullYear();
-      var aniv =  new Date(2023,dados[i].dob.date.getMonth(),dados[i].dob.date.getDate())
-      var dias = Math.floor((aniv-DiaAtual) / (1000 * 60 * 60 * 24))
-      dados[i].dob.date.faltantes = dias;
-        if(dias>=0){
-          dados[i].dob.aniversario=dias+1;
-        }else{
-        dados[i].dob.aniversario=(dias+365)+1;
-        }
+  for(var i=0; i<dados.length;i++){
+    results.push(dados[i].results)
+        
     }
-    atualizar();
 })
 .catch(error => console.error('Erro:', error));
 
@@ -30,65 +22,13 @@ $('.add-user').on('click', function () {
       $("#message-add").fadeIn();
       setTimeout(function () {
         $("#message-add").fadeOut();
-      }, 2000);
-    })
-    .then(data =>{
-      location.reload();
+      },
+       2000);
+       setTimeout(function () {
+       location.reload();
+       },2000);
     })
     .catch(error => console.error('Erro:', error));
 })
-//evento para filtrar a tabela home, variando por idade, city, ou cpf da pessoa//
-$("#btnsearch").on('click', function () {
-  var value = $('.pesquisar').val();
-    $.ajax({
-      type: 'POST',
-      url: '/search',
-      contentType: 'application/json',
-      data: JSON.stringify({ objeto: dados, valor: value }),
-      success: function (datas) {
-        console.log(datas);
-      },
-      error: function (xhr, status, error) {
-        console.error('Falha na solicitação:', error);
-      }
-    })
-});
-//evento enter do input de pesquisa//
-$(document).ready(function () {
-  $(".pesquisar").keyup(function (e) {    
-    if (e.keyCode === 13) {
-    $("#btnsearch").click();
-    }
-  });
-});
 
-//funcao para atualizar os dados com as novas coordenadas no backend//
-function atualizar(){
-  $.ajax({
-    type: 'POST',
-    url: '/atualizar',
-    contentType: 'application/json',
-    data: JSON.stringify(dados),
-    error: function (xhr, status, error) {
-      console.error('Falha na solicitação:', error);
-    }
-  })
-}
-//ordena a tabela por faltante//
-function ordenarTabelaPorDia() {
-  var $tabela = $('.table-aniver');
-  var linhas = $tabela.find('tr').get();
-  var cabecalho = linhas.shift();
-  linhas.sort(function(a, b) {
-    var diaA = parseInt($(a).find('td:last-child').text());
-    var diaB = parseInt($(b).find('td:last-child').text());
-    return diaA - diaB;
-  });
-  bodyAniver.innerHTML="";
 
-  $tabela.append(cabecalho);
-  $.each(linhas, function(index, linha) {
-    
-    $tabela.append(linha);
-  });
-}
